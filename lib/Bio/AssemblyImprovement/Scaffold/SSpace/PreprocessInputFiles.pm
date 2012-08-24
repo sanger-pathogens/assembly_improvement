79a9cd2187a478f0c8e584089358723c72eec8be
andrewjpage <andrewjpage@gmail.com>
2012-08-24 14:57:07 +0100

    run abacas as a standalone

--- lib/Bio/AssemblyImprovement/Scaffold/SSpace/PreprocessInputFiles.pm
+++ lib/Bio/AssemblyImprovement/Scaffold/SSpace/PreprocessInputFiles.pm
@@ -27,16 +27,17 @@ with 'Bio::AssemblyImprovement::Scaffold::SSpace::TempDirectoryRole';
 with 'Bio::AssemblyImprovement::Abacas::DelimiterRole';
 
 has 'input_assembly' => ( is => 'ro', isa => 'Str',      required => 1 );
-has 'input_files'    => ( is => 'ro', isa => 'ArrayRef', required => 1 );
+has 'input_files'    => ( is => 'ro', isa => 'Maybe[ArrayRef]', required => 1 );
 has 'reference'      => ( is => 'ro', isa => 'Maybe[Str]' );
 
 has 'processed_input_assembly' => ( is => 'ro', isa => 'Str',        lazy => 1, builder => '_build_processed_input_assembly' );
-has 'processed_input_files'    => ( is => 'ro', isa => 'ArrayRef',   lazy => 1, builder => '_build_processed_input_files' );
+has 'processed_input_files'    => ( is => 'ro', isa => 'Maybe[ArrayRef]',   lazy => 1, builder => '_build_processed_input_files' );
 has 'processed_reference'      => ( is => 'ro', isa => 'Maybe[Str]', lazy => 1, builder => '_build_processed_reference' );
 
 sub _build_processed_input_files {
     my ($self) = @_;
     my @processed_input_files;
+    return undef unless(defined($self->processed_input_files));
 
     for my $filename ( @{ $self->input_files } ) {
         push( @processed_input_files, $self->_gunzip_file_if_needed($filename) );
