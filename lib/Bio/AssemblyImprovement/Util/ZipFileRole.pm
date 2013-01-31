8cc3e0aa895174406d2422cbbf158ed6332a7169
nds <nds@sanger.ac.uk>
2013-01-31 16:35:52 +0000

    Script to run sga

---
+++ lib/Bio/AssemblyImprovement/Util/ZipFileRole.pm
@@ -0,0 +1,34 @@
+package Bio::AssemblyImprovement::Util::ZipFileRole;
+# ABSTRACT: Zip file
+
+=head1 SYNOPSIS
+
+Role for zipping files (not tested yet)
+
+	with 'Bio::AssemblyImprovement::Util::ZipFileRole';
+   
+=cut
+
+use Moose::Role;
+use IO::Compress::Gzip qw(gzip $GzipError) ;
+use Cwd qw(abs_path);
+use Cwd;
+use File::Basename;
+
+sub _zip_file {
+  
+    my ( $self, $input_filename ) = @_;
+    
+	return undef unless(defined($input_filename));
+	
+    my $output_directory = abs_path (getcwd()); 
+    
+    my $filename = fileparse( $input_filename );
+    my $output_filename = join( '/', ( $output_directory, $filename.'.gz' ) );
+    gzip $input_filename => $output_filename or die "gzip failed: $GzipError\n";
+    return $output_filename;
+
+}
+
+no Moose;
+1;
