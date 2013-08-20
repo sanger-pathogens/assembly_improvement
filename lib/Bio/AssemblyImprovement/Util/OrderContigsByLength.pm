d34f6663a8e1106b5bb41551f844fbd91285e990
Craig Porter <cp7@sanger.ac.uk>
2013-08-20 10:39:11 +0100

    added sort contigs module

---
+++ lib/Bio/AssemblyImprovement/Util/OrderContigsByLength.pm
@@ -0,0 +1,107 @@
+package Bio::AssemblyImprovement::Util::OrderContigsByLength;
+
+# ABSTRACT: Sorts a fasta file by sequence length
+
+=head1 SYNOPSIS
+
+
+=cut
+
+use Moose;
+use Bio::SeqIO;
+use File::Basename;
+use File::Copy;
+
+with 'Bio::AssemblyImprovement::Scaffold::SSpace::TempDirectoryRole';
+
+has 'input_filename'        => ( is => 'ro', isa => 'Str',        required => 1       );
+has 'contig_basename'       => ( is => 'rw', isa => 'Maybe[Str]', default => undef    );
+has 'rename_contigs'        => ( is => 'rw', isa => 'Bool',       default => 1        );
+has 'output_filename'       => ( is => 'rw', isa => 'Str',        lazy_build => 1     );
+has '_output_prefix'        => ( is => 'ro', isa => 'Str',        default => "sorted" );
+has '_temp_output_filename' => ( is => 'ro', isa => 'Str',        lazy_build => 1     );
+has '_input_fh'             => ( is => 'ro', isa => 'Bio::SeqIO', lazy_build => 1     );
+has '_output_fh'            => ( is => 'ro', isa => 'Bio::SeqIO', lazy_build => 1     );
+
+sub _build_output_filename
+{
+    my ($self) = @_;
+    my ( $filename, $directories, $suffix ) = fileparse( $self->input_filename, qr/\.[^.]*/ );
+    $directories . $filename . "." . $self->_output_prefix . $suffix;    
+}
+
+sub _build__temp_output_filename
+{
+    my ($self) = @_;
+    my ( $filename, $directories, $suffix ) = fileparse( $self->input_filename, qr/\.[^.]*/ );
+    return join('/',($self->_temp_directory,$filename.'.temp.fa'));
+}
+
+sub _build__input_fh 
+{
+    my ($self) = @_;
+    return Bio::SeqIO->new( -file => $self->input_filename, -format => 'Fasta' );
+}
+
+sub _build__output_fh
+{
+    my ($self) = @_;
+    return Bio::SeqIO->new( -file => "+>" . $self->_temp_output_filename, -format => 'Fasta' );
+}
+
+sub _rename_contig
+{
+    my ($self,$seq_name,$contig_number) = @_;
+
+    if( defined $self->contig_basename )
+    {
+        # rename contig
+        return $self->contig_basename.$contig_number;
+    }
+    elsif( $seq_name =~ m/^(NODE_)\d+(\D?.*)$/i )
+    {
+        # renumber velvet/spades contig
+        return $1.$contig_number.$2;
+    }
+    elsif( $seq_name =~ m/^(scaffold)\d+(\D?.*)$/i )
+    {
+        # renumber scaffold contig
+        return $1.$contig_number.$2;
+    }
+    else
+    {
+        # append contig number to contig name
+        return join('.',($seq_name,$contig_number));
+    }
+}
+
+sub run
+{
+    my ($self) = @_;
+    
+    # get sequences
+    my @sequences;
+    while( my $seq = $self->_input_fh->next_seq() )
+    {
+        push @sequences, $seq;
+    }
+
+    # sort sequences
+    @sequences = sort {$b->length <=> $a->length} @sequences;
+
+    # write to temp file
+    my $count = 1;
+    for my $seq (@sequences)
+    {
+        $seq->display_id($self->_rename_contig($seq->display_id,$count)) if $self->rename_contigs;
+        $self->_output_fh->write_seq( $seq );
+        $count++;
+    }
+
+    # move sorted temp file to outputfile
+    move($self->_temp_output_filename, $self->output_filename);
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
---
+++ t/Util/OrderContigsByLength.t
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use File::Temp;
+use File::Copy;
+
+BEGIN { unshift( @INC, './lib' ) }
+
+BEGIN {
+    use Test::Most;
+    use Test::File::Contents;
+    use_ok('Bio::AssemblyImprovement::Util::OrderContigsByLength');
+}
+
+my $data_dir              = 't/data';
+my $input_file            = 'contigs_needing_sorted.fa';
+my $output_filename       = 'contigs_needing_sorted.sorted.fa';
+my $expected_sorted_file  = 'expected_contigs_needing_sorted.fa';
+
+# work in temp directory
+my $temp_directory_obj = File::Temp->newdir( CLEANUP => 1 );
+my $temp_directory = $temp_directory_obj->dirname();
+copy(join('/',($data_dir,$input_file)), $temp_directory);
+
+# instantiate
+ok my $sort_contigs = Bio::AssemblyImprovement::Util::OrderContigsByLength->new( input_filename  => join('/',($temp_directory,$input_file)) ), 'instantiate object';
+is $sort_contigs->output_filename(), join('/',($temp_directory,$output_filename)), 'output filename correct';
+
+# sort config
+ok $sort_contigs->run(), 'sort contigs';
+files_eq($sort_contigs->output_filename(),join('/',($data_dir,$expected_sorted_file)));
+
+# check renumber function
+is $sort_contigs->_rename_contig('NODE_3_length_14_cov_10.00',99), 'NODE_99_length_14_cov_10.00', 'renamed velvet/spades contig';
+is $sort_contigs->_rename_contig('NODE_3',99), 'NODE_99', 'renamed NODE_ contig';
+is $sort_contigs->_rename_contig('NODE_3',99), 'NODE_99', 'renamed NODE_ contig';
+is $sort_contigs->_rename_contig('scaffold3|size14',99), 'scaffold99|size14', 'renamed scaffold with size contig';
+is $sort_contigs->_rename_contig('scaffold3',99), 'scaffold99', 'renamed scaffold contig';
+is $sort_contigs->_rename_contig('random_contig_name',99), 'random_contig_name.99', 'renamed contig';
+ok $sort_contigs->contig_basename('sorted'), 'set contig basename';
+is $sort_contigs->_rename_contig('NODE_3_length_14_cov_10.00',99), 'sorted99', 'renamed contig with new contig basename';
+
+done_testing();
