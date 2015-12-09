828c6b1c173679b107b4e7ec2415a22c23cdee9d
nds <nds@sanger.ac.uk>
2015-12-09 14:51:08 +0000

    create dummy bam files

--- t/dummy_quiver_script.pl
+++ t/dummy_quiver_script.pl
@@ -5,6 +5,8 @@ my $directory_name = "tmp_quiver";
 system("mkdir tmp_quiver");
 system("mkdir tmp_quiver/All_output");
 system("touch tmp_quiver/consensus.fasta");
+system("touch tmp_quiver/aligned_reads.bam");
+system("touch tmp_quiver/aligned_reads.bam.bai");
 system("touch tmp_quiver/run-assembly.sh");
 system("touch tmp_quiver/All_output/input.xml");
 system("touch tmp_quiver/All_output/settings.xml");
